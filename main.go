6056fa8a028b2bc93a41e40f56d3531158230a4c
Kushagra-Gupta01 <kushagra.gupta0077@gmail.com>
2023-12-14 21:31:12 +0530

    handler funcs added

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"net/http"
 	"github.com/kushagra-gupta01/Cryto_Marketplace/orderbook"
 	"github.com/labstack/echo/v4"
 )
@@ -9,10 +11,18 @@ func main(){
 
 	e := echo.New()
 	ex := NewExchange()
+	e.GET("/book/:market",ex.handleGetBook)
 	e.POST("/order",ex.handlePlaceOrder)
 	e.Start(":3000")
 
 }
+
+type OrderType string
+
+const(
+	MarketOrder OrderType = "MARKET"
+	LimitOrder OrderType = "LIMIT"
+)
 
 type Market string
 
@@ -25,11 +35,84 @@ type Exchange struct{
 }
 
 func NewExchange() *Exchange{
+	orderbooks := make(map[Market]*orderbook.OrderBook)
+	orderbooks[MarketETH] = orderbook.NewOrderBook()
+	
 	return &Exchange{
-		orderbooks: make(map[Market]*orderbook.OrderBook),
+		orderbooks: orderbooks,
 	}
 }
 
+type PlaceOrderRequest struct{
+	Type 	OrderType //market or limit
+	Bid		bool
+	Size 	float64
+	Price float64
+	Market Market
+}
+
+type Order struct{
+	Price float64
+	Size float64
+	Bid bool
+	TimeStamp int64
+}
+
+type OrderBookData struct{
+	Asks []*Order
+	Bids []*Order
+}
+
+func(ex *Exchange) handleGetBook(c echo.Context)error{
+	market := Market(c.Param("market"))
+	ob,ok := ex.orderbooks(market)
+	if !ok{
+		return c.JSON(http.StatusBadRequest,map[string]any{"msg":"message not found"})
+	}
+
+	orderbookData := OrderBookData{
+		Asks: []*Order{},
+		Bids: []*Order{},
+	}
+
+	for _,limit := range ob.Asks(){
+		for _,order := range limit.Orders{
+			o := Order{
+				Price: limit.Price,
+				Size: order.Size,
+				Bid: order.Bid,
+				TimeStamp: order.TimeStamp,
+			}
+			orderbookData.Asks = append(orderbookData.Asks,&o)
+		}
+	}
+
+	for _,limit := range ob.Bids(){
+		for _,order := range limit.Orders{
+			o := Order{
+				Price: limit.Price,
+				Size: order.Size,
+				Bid: order.Bid,
+				TimeStamp: order.TimeStamp,
+			}
+			orderbookData.Bids = append(orderbookData.Bids,&o)
+		}
+	}
+
+	return c.JSON(http.StatusOK,orderbookData)
+}
+
 func(ex *Exchange) handlePlaceOrder(c echo.Context) error{
-	return c.JSON(200,"f4g")
+	var placeOrderData PlaceOrderRequest
+
+	if err:= json.NewDecoder(c.Request().Body).Decode(&placeOrderData);err!=nil{
+		return err
+	}
+
+	market:= Market(placeOrderData.Market)
+	ob := ex.orderbooks[market]
+	order := orderbook.NewOrder(placeOrderData.Bid,placeOrderData.Size)
+	ob.PlaceLimitOrder(placeOrderData.Price,order)
+
+	return c.JSON(200,map[string]any{"msg":"order placed"})
 }
